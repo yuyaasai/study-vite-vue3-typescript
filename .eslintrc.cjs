5bd0c76b2df44e48d6bbdd516e23f9ba8be57c42
yuyaasai <74097794+yuyaasai@users.noreply.github.com>
2022-12-07 18:53:06 +0900

    eslint + prettier の導入

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution")
+
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        "standard-with-typescript",
+        "plugin:vue/vue3-recommended",
+        "@vue/eslint-config-typescript/recommended",
+        "prettier"
+    ],
+    overrides: [],
+    plugins: ["vue", "@typescript-eslint"],
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        extraFileExtensions: [".vue"],
+        parser: "@typescript-eslint/parser",
+        project: "./tsconfig.eslint.json",
+        sourceType: "module",
+        tsconfigRootDir: __dirname
+    }
+}
